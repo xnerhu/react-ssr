700f1b80acef9356b6e85970892c2eeb6fab6fdf
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-10-19 20:24:14 +0200

    feat: add static directory

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,11 +1,12 @@
 import { Router, static as staticDir } from 'express';
-import { join } from 'path';
+import { join, resolve } from 'path';
 
 import renderer from './renderer';
 
 const router = Router();
 
 router.use('/static', staticDir(join('build/client', 'static')));
+router.use('/static', staticDir(resolve('static')));
 router.use(renderer);
 
 export default router;
